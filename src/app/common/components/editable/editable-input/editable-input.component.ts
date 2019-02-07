529d3968bf0d68e1fac7d3ceafc3f59c7665e474
k1cho <aadnanhadzipasic@gmail.com>
2019-02-07 20:23:05 +0100

    added update rental component

---
+++ src/app/common/components/editable/editable-input/editable-input.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-editable-input',
+  templateUrl: './editable-input.component.html',
+  styleUrls: ['./editable-input.component.scss']
+})
+export class EditableInputComponent implements OnInit {
+  @Input() entity: any;
+  @Input() field: string;
+  isActiveInput = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
